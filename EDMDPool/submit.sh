543adb4e757fd43327b8a82fa39e6203a4ea4509
wakawakaer <47529673+wakawakaer@users.noreply.github.com>
2022-11-10 11:28:01 +0800

    Update submit.sh

--- EDMDPool/submit.sh
+++ EDMDPool/submit.sh
@@ -6,3 +6,14 @@
 #JSUB -o ./result/output.%J
 #JSUB -J andi
 ./run_EDMDPool.sh  PROTEINS 0 0
+
+
+:<<!
+    ./run_EDMDPool.sh DATA FOLD GPU
+to run on dataset using fold number (1-10).
+You can run
+    ./run_EDMDPool.sh DD 0 0
+to run on DD dataset with 10-fold cross validation on GPU #0.
+    ./run_EDMDPool.sh COLLAB 1 1
+to run on COLLAB dataset with first fold on GPU #1.
+!
